8e078fd85533363ca12f573325374a32065b8d41
Doris Flaiz <doris.flaiz@jos-group.com>
2025-10-20 20:18:42 -0700

    Fix typo in JSON response handling

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ from snowflake.snowpark.functions import col
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 #st.text(smoothiefroot_response.json())
-sf-df = st.dataframe(data=smoothiefroot_response.jason(), use_container_width=True)
+sf-df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 # Write directly to the app
 st.title('My Parents New Healthy Diner') 
